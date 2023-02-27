7e023d30c8b6bfdca73c31f5db4ca7938c3450f6
Miriam <miriamjoshua00@gmail.com>
2023-02-26 22:50:43 -0500

    car list with cards

--- client/src/components/Cars/Cars.js
+++ client/src/components/Cars/Cars.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchCars } from '../../redux/carsSlice';
+import CarCard from './CarCard';
 import NewCarForm from './NewCarForm';
 import SearchBar from '../Search/SearchBar';
+import { Box, Button, Paper, Grid } from '@mui/material';
+import { styled } from '@mui/material/styles';
 
 const Cars = () => {
 
@@ -16,82 +19,12 @@ const Cars = () => {
     useEffect(() => {
         dispatch(fetchCars())
     }, [cars.length, dispatch, loading])
-
-    function addKidToCar(kid, carId) {
-        const carpoolData = {
-            kid_id: kid.id,
-            car_id: carId,
-            status: "pending"
-        }
-        fetch('/carpools', {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(carpoolData)
-                })
-            .then((r) => r.json())
-            .then((data) => {
-                if (data.error) {
-                    alert(data.error)
-                } else {
-                    dispatch(fetchCars())
-                }
-            })
-    }
-
-    function removeKidFromCar(kid, carId) {
-        const carpoolData = {
-            kid_id: kid.id,
-            car_id: carId
-        }
-        fetch(`/carpools/delete`, { 
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(carpoolData) })
-            
-            .then((r) => r.json())
-            .then((data) => {
-                dispatch(fetchCars())
-            })
-    }
    
     const mappedCars = cars && cars.map((car) => {
-        
-        const mappedKids = kids && kids.map((kid) => {
-         return  <button key={kid.id} onClick={() => addKidToCar(kid, car.id)}>Add {kid.name} to this car</button>   
-        })
-        
-        const time = car.dismissal_time && car.dismissal_time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
-
-        const mappedCarKids = car.kids.map((kid) => {
-            if(kids && kids.find(k => k.id === kid.id)) {
-            return <button key={kid.id} onClick={() => removeKidFromCar(kid, car.id)}>Remove {kid.name} from this car</button> 
-            }
-         })
-       
         return(
-            <div key={car.id}>
-                <p>Driver: {car.user && car.user.name}</p>
-                <p>Seats Available: {car.seats_available}</p>
-                <p>School: {car.school}</p>
-                <p>Time: {time}</p>
-                <p>Kids in Carpool: {car.kids && car.kids.map((kid, index) => <span key={index}>{kid.name} </span>)}</p>
-                <ul>
-                    <h5>Days</h5>
-                    { car.monday === "monday" ? <p>Monday</p> : null }
-                    { car.tuesday === "tuesday" ? <p>Tuesday</p> : null }
-                    { car.wednesday === "wednesday" ? <p>Wednesday</p> : null }
-                    { car.thursday === "thursday" ? <p>Thursday</p> : null }
-                    { car.friday === "friday" ? <p>Friday</p> : null }
-                </ul>
-                <div>
-                     {mappedKids}
-                    {mappedCarKids}
-                </div>
-            </div>
+        <Grid item style={{display: 'flex'}}>
+            <CarCard car={car} kids={kids}/>
+        </Grid>  
         )
     })
 
@@ -100,16 +33,24 @@ const Cars = () => {
     }
 
   return (
-    <div>
-        <div>
+    <Box sx={{ flexGrow: 1 }}>
+        
+
+            <div>
             <SearchBar />    
         </div>
-        <ul>
-            {mappedCars}
-        </ul>
-        <button onClick={toggleCarForm}>Add New Car</button>
+        <Button onClick={toggleCarForm}>Add New Car</Button>
         { showForm ? <NewCarForm showForm={showForm} setShowForm={setShowForm} /> : null }
-    </div>
+        <Grid 
+        container 
+        spacing={4}
+        direction="row"
+        justifyContent="space-evenly"
+        alignItems="stretch"
+        >
+            {mappedCars}
+        </Grid>
+    </Box>
   )
 }
 
